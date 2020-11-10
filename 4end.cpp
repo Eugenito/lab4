1b6fe75fffd9902c6373d309e5e237ccd7de724b
Eugenito <72038188+Eugenito@users.noreply.github.com>
2020-11-10 23:40:40 +0300

    Add files via upload

---
+++ 4end.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int a[10] = { 1,3,5,6,10,11,12,13,4,7 };
+	int quantity = 0;
+	for (int i = 0;i < 10;i++)
+	{
+		if (a[i + 1] < a[i])
+		{
+			quantity += 1;
+		}
+	}
+	printf("%d", quantity);
+	return 0;
+}
